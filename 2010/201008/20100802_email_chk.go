8368d03b350908ffd06851439fd13d0a1f23a927
bonly <bonly@163.com>
2013-01-11 15:17:25 +0800

    mv 201009 to dir

--- 2010/201008/20100802_email_chk.go
+++ 2010/201008/20100802_email_chk.go
@@ -1,3 +1,11 @@
+/*
+中文判断:regexp.MatchString("^[\\x{4e00}-\\x{9fa5}]+$", r.Form.Get("realname"));
+英文判断:regexp.MatchString("^[a-zA-Z]+$", r.Form.Get("engname"));
+邮箱:regexp.MatchString(`^([\w\.\_]{2,10})@(\w{1,}).([a-z]{2,4})$`, r.Form.Get("email"));
+手机号码:regexp.MatchString(`^(1[3|4|5|8][0-9]\d{4,8})$`, r.Form.Get("mobile"));
+15位身份证:regexp.MatchString(`^(\d{15})$`, r.Form.Get("usercard")); 
+18位身份证:regexp.MatchString(`^(\d{17})([0-9]|X)$`, r.Form.Get("usercard"));
+*/
 package main;
 import (
 "flag"
